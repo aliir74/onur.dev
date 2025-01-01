b84066f3f99148cee9b311195d30306c52322ae2
Ali Irani <ali.irani@ovou.com>
2025-01-01 17:59:08 +0330

    feat: update profiles and navigation icons in constants.js
    - Added new profiles for YouTube and Telegram with corresponding icons.
    - Replaced SparklesIcon with Home icon in the navigation links.
    - Removed commented-out sections for unused social media profiles to clean up the code.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-import { GithubIcon, LinkedinIcon, SparklesIcon } from 'lucide-react'
+import { GithubIcon, LinkedinIcon, Home, YoutubeIcon } from 'lucide-react'
 
 export const PROFILES = {
   linkedin: {
@@ -6,64 +6,29 @@ export const PROFILES = {
     url: 'https://www.linkedin.com/in/aliirani',
     icon: <LinkedinIcon size={16} />
   },
+  youtube: {
+    title: 'YouTube',
+    url: 'https://www.youtube.com/@aliir74',
+    icon: <YoutubeIcon size={16} />
+  },
   github: {
     title: 'GitHub',
     url: 'https://github.com/aliir74',
     icon: <GithubIcon size={16} />
+  },
+  telegram: {
+    title: 'Telegram',
+    url: 'https://t.me/+3CqRmaAHDj5lOTE0',
+    icon: (
+      <svg width="16px" height="16px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M14.5 1.5L0.5 6.5L4.5 8.5L10.5 4.5L6.5 9.5L12.5 13.5L14.5 1.5Z"
+          stroke="#000000"
+          strokeLinejoin="round"
+        />
+      </svg>
+    )
   }
-  // twitter: {
-  //   title: 'X (Twitter)',
-  //   username: 'onurschu',
-  //   url: 'https://twitter.com/intent/user?screen_name=onurschu',
-  //   icon: (
-  //     <svg
-  //       xmlns="http://www.w3.org/2000/svg"
-  //       className="size-4"
-  //       width="44"
-  //       height="44"
-  //       viewBox="0 0 24 24"
-  //       strokeWidth="1.5"
-  //       stroke="#000000"
-  //       fill="none"
-  //       strokeLinecap="round"
-  //       strokeLinejoin="round"
-  //     >
-  //       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-  //       <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
-  //       <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
-  //     </svg>
-  //   )
-  // },
-  // medium: {
-  //   title: 'Medium',
-  //   url: 'https://suyalcinkaya.medium.com'
-  // },
-  // instagram: {
-  //   title: 'Instagram',
-  //   url: 'https://www.instagram.com/jgrmn',
-  //   icon: <InstagramIcon size={16} />
-  // },
-  // soundcloud: {
-  //   title: 'Soundcloud',
-  //   url: 'https://soundcloud.com/jagerman'
-  // },
-  // youtube: {
-  //   title: 'YouTube',
-  //   url: 'https://www.youtube.com/c/jagermanmusic',
-  //   icon: <YoutubeIcon size={16} />
-  // },
-  // bluesky: {
-  //   title: 'Bluesky',
-  //   url: 'https://staging.bsky.app/profile/onur.dev'
-  // },
-  // readcv: {
-  //   title: 'Read.cv',
-  //   url: 'https://read.cv/onur'
-  // },
-  // pinterest: {
-  //   title: 'Pinterest',
-  //   url: 'https://nl.pinterest.com/onurschu'
-  // }
 }
 
 export const TWEETS_COLLECTION_ID = 15896982
@@ -88,7 +53,7 @@ export const LINKS = [
   {
     href: '/',
     label: 'Home',
-    icon: <SparklesIcon size={16} />
+    icon: <Home size={16} />
   }
   // {
   //   href: '/writing',
